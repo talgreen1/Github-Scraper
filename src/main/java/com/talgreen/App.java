720d61269cca6951b28b37f62b1497a9d87a44a6
talgreen <talgreen1@yahoo.com>
2019-02-19 17:47:38 +0200

    Allow to overwrite parameters from command line

--- src/main/java/com/talgreen/App.java
+++ src/main/java/com/talgreen/App.java
@@ -1,29 +1,10 @@
 package com.talgreen;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.openqa.selenium.WebElement;
-
 import java.net.MalformedURLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class App {
 
-//    public static void main(String[] args) {
-//        DAO dao = new DAO();
-//        GithubParser parser = new GithubParser();
-//
-//        List<Repository> repos = new ArrayList<>();
-//        repos.add(new Repository("A", "AA"));
-//        repos.add(new Repository("B", "BB"));
-//        repos.add(new Repository("C", "CC"));
-//
-//        parser.setRepositories(repos);
-//        dao.save(parser);
-//    }
     public static void main(String[] args) throws MalformedURLException {
         DAO dao = new DAO();
         GithubParser parser = new GithubParser();
@@ -37,15 +18,7 @@ public class App {
         repositories.forEach(System.out::println);
 
         dao.save(parser);
-        //parser.saveRepositoriesToDb();
-//        List<WebElement> repositoriesWebElements = parser.getRepositoriesWebElements();
-//
-//        Repository repository = new RepositoryBuilder().buildFromRepositoryWebElement(repositoriesWebElements.get(0));
-//
-//        System.out.println(repository);
 
         parser.closeGithub();
-
-
     }
 }
--- src/main/java/com/talgreen/Props.java
+++ src/main/java/com/talgreen/Props.java
@@ -9,10 +9,13 @@ public class Props {
     public static final String SELENIUM_URL;
     public static final String GITHUB_HOMEPAGE;
 
+    private static final String SELENIUM_URL_KEY = "seleniumUrl";
+    private static final String SELENIUM_PORT_KEY = "seleiumPort";
+    private static final String GITHUB_HOMEPAGE_KEY = "githubHomepage";
+
     static {
         String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         String appConfigPath = rootPath + "app.properties";
-        //String catalogConfigPath = rootPath + "catalog";
 
         Properties appProps = new Properties();
         try {
@@ -21,9 +24,10 @@ public class Props {
             e.printStackTrace();
         }
 
-        SELENIUM_URL = appProps.getProperty("seleniumUrl");
-        SELENIUM_PORT=Integer.parseInt(appProps.getProperty("seleiumPort"));
-        GITHUB_HOMEPAGE = appProps.getProperty("githubHomepage");
+        SELENIUM_URL = System.getProperty(SELENIUM_URL_KEY, appProps.getProperty(SELENIUM_URL_KEY));;
+        SELENIUM_PORT=Integer.parseInt(
+                System.getProperty(SELENIUM_PORT_KEY, appProps.getProperty(SELENIUM_PORT_KEY)));
+        GITHUB_HOMEPAGE = System.getProperty(GITHUB_HOMEPAGE_KEY, appProps.getProperty(GITHUB_HOMEPAGE_KEY));
     }
 
 }
